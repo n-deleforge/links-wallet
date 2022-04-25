efabf130d9f1d8ae42bfc5da257c21330b52aa4d
n-deleforge <contact@nicolas-deleforge.fr>
2022-04-25 15:26:31 +0200

    webâck + first migration

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,11 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail("hello@nicolas-deleforge.fr");
+        $user->setPassword("password");
 
+        $manager->persist($user);
         $manager->flush();
     }
 }
